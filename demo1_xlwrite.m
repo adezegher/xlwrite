0f4d7b433cd9e2bf8f95dc05ddb90d1a80800296
Alec de Zegher <alecweb+github@gmail.com>
2017-03-16 16:19:08 +0100

    Improves the name of the demo and the explanation.
    Fixes #1

--- demo1_xlwrite.m
+++ demo1_xlwrite.m
@@ -1,7 +1,13 @@
-%% Small demonstration on how to use XLWRITE
+%% Demo on how to use XLWRITE to save data to a xlsx file called `demo1_xlwrite.xlsx`
+% It will create a new file if the file `demo1_xlwrite.xlsx` does not exist yet.
+% It will overwrite the content of the cells if the file already exists.
 
 %% Initialisation of POI Libs
-% Add Java POI Libs to matlab javapath
+% Adding required libraries for xlwrite to work correctly.
+%
+% IMPORTANT NOTE:
+% xlwrite needs the below libraries to be loaded/executed once per session.
+% Update the paths below if this script is not in the same folder as the `poi_library` folder.
 javaaddpath('poi_library/poi-3.8-20120326.jar');
 javaaddpath('poi_library/poi-ooxml-3.8-20120326.jar');
 javaaddpath('poi_library/poi-ooxml-schemas-3.8-20120326.jar');
@@ -11,7 +17,7 @@ javaaddpath('poi_library/stax-api-1.0.1.jar');
 
 %% Data Generation for XLSX
 % Define an xls name
-fileName = 'test_xlwrite.xlsx';
+fileName = 'demo1_xlwrite.xlsx';
 sheetName = 'this_is_sheetname';
 startRange = 'B3';
 
